7c2fa92c939653131977873e180d1509df12e715
Berk Ustun <ustunb@mit.edu>
2019-03-03 14:30:03 -0500

    fixed broken paths

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,12 +1,13 @@
-import os
+from pathlib import Path
 
-# infer core directories
-pkg_dir = os.path.dirname(os.path.abspath(__file__))
-repo_dir = os.path.dirname(pkg_dir)
-data_dir = os.path.join(repo_dir, 'data')
-results_dir = os.path.join(repo_dir, 'results')
+repo_dir = Path(__file__).absolute().parent.parent
+
+# directory containing data files
+data_dir = repo_dir / 'data/'
+
+# directory containing results
+results_dir = repo_dir / 'results/'
 
 # create directories that don't exist
 for d in [data_dir, results_dir]:
-    if not os.path.exists(d):
-        os.makedirs(d)
+    d.mkdir(exist_ok = True)
