cd8c29e2d2b3ed6df964f9610853bc6818470da4
Berk Ustun <ustunb@mit.edu>
2019-03-08 00:31:53 -0500

    removed useless methods; added specs; _repr_, _str_

--- recourse/flipset.py
+++ recourse/flipset.py
@@ -6,100 +6,107 @@ from recourse.helper_functions import parse_classifier_args
 from recourse.action_set import ActionSet
 from recourse.builder import RecourseBuilder
 
-# todo disable displays in Flipset builder
-
 class Flipset(object):
+    """
+    List of actions that will flip the predicted value of a classifier from x
+    """
+    valid_enumeration_types = RecourseBuilder._valid_enumeration_types
+    valid_cost_types = RecourseBuilder._valid_mip_cost_types
 
-    df_column_names = ['names',
-                       'idx',
+    df_column_names = ['cost',
                        'size',
-                       'total_cost',
-                       'start_values',
-                       'final_values',
-                       'final_score',
-                       'final_prediction',
+                       'features',
+                       'feature_idx',
+                       'x',
+                       'x_new',
+                       'score_new',
+                       'yhat_new',
                        'feasible',
                        'flipped']
 
+
     def __init__(self, x, action_set, **kwargs):
 
-        assert isinstance(x, (list, np.ndarray))
-        x = np.array(x, dtype = np.float_).flatten()
+        # attach action set
         assert isinstance(action_set, ActionSet)
         self.action_set = action_set
-        self._x = x
         self._n_variables = len(action_set)
         self._variable_names = action_set.name
+
+        # attach feature vector
+        assert isinstance(x, (list, np.ndarray))
+        self._x = np.array(x, dtype = np.float_).flatten()
+
+        # attach coefficients
         self._coefs, self._intercept = parse_classifier_args(**kwargs)
+
+        # initialize Flipset attributes
+        self._builder = None
         self._items = []
         self._df = pd.DataFrame(columns = Flipset.df_column_names, dtype = object)
-        self._sort_args = {'by': ['size', 'total_cost', 'final_score'], 'inplace': True, 'axis': 0}
+        self._sort_args = {'by': ['size', 'cost', 'score_new'], 'inplace': True, 'axis': 0}
 
 
+    ### built-ins ###
+    def __len__(self):
+        """
+        :return: # of items in the flipset
+        """
+        return len(self._items)
+
+
+    def __str__(self):
+        return str(self._df[['cost', 'size', 'features', 'x', 'x_new']])
+
+
+    def __repr__(self):
+        s = ['Flipset with %d Items',
+             '# items: %d' % len(self),
+             'x: %r' % self._x,
+             'w: (%s)' % self._coefs,
+             'items: %r' % self._items]
+        return '\n'.join(s)
+
+
+    ### properties ###
     @property
     def x(self):
+        """
+        :return: feature vector
+        """
         return self._x
 
 
     @property
-    def df(self):
-        return self._df
-
-
-    @property
-    def sort_args(self):
-        return self._sort_args
-
-
-    def sort(self, **sort_args):
-
-        if len(sort_args) == 0:
-            self._df.sort_values(**self._sort_args)
-            return
-
-        if 'by' in sort_args:
-            sort_names = sort_args['by']
-        else:
-            sort_names = list(sort_args.keys())
-            sort_args = {}
-
-        assert isinstance(sort_names, list)
-        assert len(sort_names) > 0
-        for s in sort_names:
-            assert isinstance(s, str)
-            assert s in self._df.columns
-
-        sort_args['by'] = sort_names
-        sort_args['inplace'] = True
-        sort_args['axis'] = 0
-
-        self._df.sort_values(**sort_args)
-        self._sort_args = sort_args
-
-
-    @property
-    def n_variables(self):
-        return self._n_variables
-
-
-    @property
-    def variable_names(self):
-        return list(self._variable_names)
-
-
-    @property
-    def coefficients(self):
-        return self._coefs
-
-
-    @property
-    def intercept(self):
-        return self._intercept
-
-
-    @property
     def items(self):
+        """
+        Dictionary representation of Flipset
+        This is a list containing mildly processed output from recourse.builder
+        """
         return self._items
+
+
+    @property
+    def df(self):
+        """
+        Pandas DataFrame representation of Flipset
+        Each row represents a different action to flip the prediction
+        Rows are sorted according to the last arguments passed to the Flipset.sort()
+
+        DESCRIPTION OF COLUMNS
+        ----------------------
+        cost:           cost of action
+        size:           # of altered features
+        features:       names of altered variables
+        feature_idx:    column indices of altered features
+        x:              values of x[j] for j in feature_idx
+        x_new:          values of x[j] + a[j]) for j in feature_idx
+        score_new:      values of score function at x_new = x + a = w0 + w.dot(x + a)
+        yhat_new:       value of prediction at x_new = x + a = f(score_new)
+        feasible:       True if x + a is a feasible action
+        flipped:        True if the f(x) != f(x+a)
+        """
+        return self._df
 
 
     @property
@@ -107,25 +114,8 @@ class Flipset(object):
         return self._intercept + np.dot(self._coefs, self._x)
 
 
-    def populate(self, total_items = 10, enumeration_type = 'distinct_subsets', mip_cost_type = 'local', time_limit = None, node_limit = None):
-        self._builder = RecourseBuilder(action_set = self.action_set, x = self.x, coefficients = self._coefs, intercept = self._intercept, mip_cost_type = mip_cost_type)
-        items = self._builder.populate(total_items = total_items, enumeration_type = enumeration_type, time_limit = time_limit, node_limit = node_limit)
-        self.add(items)
-
-
-    #### built ins ####
-
-
-    def __len__(self):
-        return len(self._items)
-
-
-    def __repr__(self):
-        return str(self._items)
-
-
     def predict(self, actions = None):
-        return np.sign(self.score(actions))
+        return np.sign(self.score(actions = actions))
 
 
     def score(self, actions = None):
@@ -135,71 +125,89 @@ class Flipset(object):
             return self._intercept + np.dot(self._coefs, self._x)
 
 
-    def add(self, items):
-        if isinstance(items, dict):
-            items = [items]
-        assert isinstance(items, list)
-        items = list(map(lambda i: self._validate_item(i), items))
-        self._items.extend(items)
-        self._update_df(items)
+    #### API functions ####
+
+    def populate(self, total_items = 10, enumeration_type = 'distinct_subsets', cost_type = 'local', time_limit = None, node_limit = None, display_flag = None):
+
+        """
+        Generates a list of actions to flip the predicted value of the linear classifier from feature vector x.
+
+        :param total_items: maximum # of actions to generate
+                            set as float('inf') to enumerate all possible actions
+
+        :param enumeration_type: enumeration algorithm to use for the flipset
+                            must be a string in Flipset.valid_enumeration_types
+                            - 'distinct_subsets'
+                            - 'mutually_exclusive'
+
+        :param cost_type: cost function to use for Flipset generation
+                          must be a string in Flipset.valid_cost_types
+                            options include:
+                            - local
+
+        :param time_limit: max # of seconds to spend before stopping the solver at each iteration
+
+        :param node_limit: max # of branch and bound nodes to process before stopping the solver at each iteration
+
+        :param display_flag: True to display solver progress during enumeration
+
+        :return:
+        """
+        if self._builder is None:
+            self._builder = RecourseBuilder(action_set = self.action_set, x = self.x, coefficients = self._coefs, intercept = self._intercept, mip_cost_type = cost_type)
+
+        items = self._builder.populate(total_items = total_items, enumeration_type = enumeration_type, time_limit = time_limit, node_limit = node_limit, display_flag = display_flag)
+        self._add(items)
 
 
-    def _validate_action(self, a):
-        a = np.array(a, dtype = np.float_).flatten()
-        assert len(a) == self.n_variables, 'action vector must have %d elements' % self.n_variables
-        assert np.all(np.isfinite(a)), 'actions must be finite'
-        assert np.count_nonzero(a) >= 1, 'at least one action element must be non zero'
-        assert np.not_equal(self.yhat, self.predict(a)), 'actions do not flip the prediction from %d' % self.yhat
-        return a
+    def sort(self, **kwargs):
+        """
+        Reorders the items in the Flipset dataframe
+        Arguments used to sort are saved
+        :param sort_args: list of fields to use in sort, or arguments passed to pd.DataFrame.sort_values
+        :return:
+        """
+        if len(kwargs) == 0:
+            self._df.sort_values(**self._sort_args)
+            return
 
+        if 'by' in kwargs:
+            sort_names = kwargs['by']
+        else:
+            sort_names = list(kwargs.keys())
 
-    def _validate_item(self, item):
-        assert isinstance(item, dict)
-        required_fields = ['feasible', 'actions', 'cost']
-        for k in required_fields:
-            assert k in item, 'item missing field %s' % k
-        item['actions'] = self._validate_action(item['actions'])
-        assert item['cost'] > 0.0, 'total cost must be positive'
-        assert item['feasible'], 'item must be feasible'
-        return item
+        assert isinstance(sort_names, list)
+        assert len(sort_names) > 0
+        for s in sort_names:
+            assert isinstance(s, str)
+            assert s in self._df.columns
 
-
-    def _item_to_df_row(self, item):
-        x = self.x
-        a = item['actions']
-        h = self.predict(a)
-        nnz_idx = np.flatnonzero(a)
-        row = {
-            'names': [self.variable_names[j] for j in nnz_idx],
-            'idx': nnz_idx,
-            'size': len(nnz_idx),
-            'start_values': x[nnz_idx],
-            'final_values': x[nnz_idx] + a[nnz_idx],
-            'cost': float(item['cost']),
-            'final_score': self.score(a),
-            'final_prediction': h,
-            'feasible': item['feasible'],
-            'flipped': np.not_equal(h, self.yhat),
+        sort_args = {
+            'by': sort_names,
+            'inplace': kwargs.get('inplace', True),
+            'axis': kwargs.get('axis', 0),
             }
-
-        return row
-
-
-    def _update_df(self, items):
-        if len(items) > 0:
-            row_data = list(map(lambda item: self._item_to_df_row(item), items))
-            self._df = self._df.append(row_data, ignore_index = True)[self._df.columns.tolist()]
-            self.sort()
+        self._df.sort_values(**sort_args)
+        self._sort_args = sort_args
 
 
     def view(self):
+        """
+        prints Flipset as Pandas dataframe
+        :return:
+        """
         return self._df
 
 
     def to_latex(self, name_formatter = '\\textit'):
+        """
+        converts current Flipset to Latex table
+        :param name_formatter:
+        :return:
+        """
 
         self.sort()
-        tex_columns = ['names', 'start_values', 'final_values']
+        tex_columns = ['features', 'x', 'x_new']
         tex_df = self._df[tex_columns]
 
         # split components for each item
@@ -227,21 +235,21 @@ class Flipset(object):
 
 
         # add another column for the latex arrow symbol
-        idx = flat_df.columns.tolist().index('final_values')
+        idx = flat_df.columns.tolist().index('x_new')
         flat_df.insert(loc = idx, column = 'to', value = ['longrightarrow'] * len(flat_df))
 
         # name headers
         flat_df = flat_df.rename(columns = {
-            'names': '\textsc{Feature Subset}',
-            'start_values': '\textsc{Current Values}',
-            'final_values': '\textsc{Required Values}'})
+            'features': '\textsc{Feature Subset}',
+            'x': '\textsc{Current Values}',
+            'x_new': '\textsc{Required Values}'})
 
         # get raw tex table
         table = flat_df.to_latex(multirow = True, index = True, escape = False, na_rep = '-', column_format = 'rlccc')
 
         # manually wrap names with a formatter function
         if name_formatter is not None:
-            for v in self.variable_names:
+            for v in self._variable_names:
                 table = table.replace(v, '%s{%s}' % (name_formatter, v))
 
         # add the backslash for the arrow
@@ -253,5 +261,81 @@ class Flipset(object):
         table[2] = table[2].replace('{}', '')
         table.pop(3)
         table.pop(3)
-
         return '\n'.join(table)
+
+
+    #### item management ####
+    def _add(self, items):
+        """
+        :param items: adds new items to flipset
+        :return:
+        """
+        if isinstance(items, dict):
+            items = [items]
+        assert isinstance(items, list)
+        items = list(map(lambda i: self._validate_item(i), items))
+        self._items.extend(items)
+        self._add_to_df(items)
+
+
+    def _validate_item(self, item):
+        """
+        checks item to be added to the current Flipset
+        :param item: raw flipset item
+        :return: item in correct format
+        """
+        assert isinstance(item, dict)
+        required_fields = ['feasible', 'actions', 'cost']
+        for k in required_fields:
+            assert k in item, 'item missing field %s' % k
+        item['actions'] = self._validate_action(item['actions'])
+        assert item['cost'] > 0.0, 'total cost must be positive'
+        assert item['feasible'], 'item must be feasible'
+        return item
+
+
+    def _validate_action(self, a):
+        """
+        checks action vector to the added to the current Flipset
+        :param a: action vector
+        :return: a or AssertionError
+        """
+        a = np.array(a, dtype = np.float_).flatten()
+        assert len(a) == self._n_variables, 'action vector must have %d elements' % self.n_variables
+        assert np.isfinite(a).all(), 'actions must be finite'
+        assert np.count_nonzero(a) >= 1, 'at least one action element must be non zero'
+        assert np.not_equal(self.yhat, self.predict(a)), 'actions do not flip the prediction from %d' % self.yhat
+        return a
+
+
+    def _add_to_df(self, items):
+        if len(items) > 0:
+            row_data = list(map(lambda item: self._item_to_df_row(item), items))
+            self._df = self._df.append(row_data, ignore_index = True, sort = True)[self._df.columns.tolist()]
+            self.sort()
+
+
+    def _item_to_df_row(self, item):
+        """
+        converts item to a row in the data frame
+        :param item:
+        :return:
+        """
+        x = self.x
+        a = item['actions']
+        h = self.predict(a)
+        nnz_idx = np.flatnonzero(a)
+        row = {
+            'cost': float(item['cost']),
+            'size': len(nnz_idx),
+            'features': [self._variable_names[j] for j in nnz_idx],
+            'feature_idx': nnz_idx,
+            'x': x[nnz_idx],
+            'x_new': x[nnz_idx] + a[nnz_idx],
+            'score_new': self.score(a),
+            'yhat_new': h,
+            'feasible': item['feasible'],
+            'flipped': np.not_equal(h, self.yhat),
+            }
+
+        return row
