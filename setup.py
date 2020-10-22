99ab1c0ee5a0dccb1e52c66f999dd4dd5e7b9d9b
Berk Ustun <ustunb@mit.edu>
2020-10-22 18:57:59 -0400

    removed e-mail from setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="actionable-recourse",
+    name="recourse",
     version="1.0.1",
     author="Berk Ustun, Alexander Spangher",
-    author_email="squall14414@gmail.com, alexander.spangher@gmail.com",
-    description="actionable-recourse is a python library for recourse verification and reporting.",
+    description="recourse is a python library for recourse verification and reporting.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ustunb/actionable-recourse",
